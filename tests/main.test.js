88675b6648986d69b40a791ec7a5962c0bdd2859
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-09 13:25:29 -0600

    test: add tests for type validation in the argument of the bini function

--- tests/main.test.js
+++ tests/main.test.js
@@ -7,3 +7,10 @@ test('bini should return a string with the result, something like \'011101 00010
 
   t.is(result, expected)
 })
+
+test('throw an error, when the argument is not of type string', t => {
+  const err = t.throws(_ => bini(45))
+
+  t.assert(err instanceof Error)
+  t.is(err.message, 'the argument must be of type string')
+})
