87af540c3f24ee72813693c23d34abe18272c9e1
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-09 13:31:42 -0600

    docs: add comments for bini()

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * function that returns a string passed by parameter in its binary representation
+ * @param {string} text text to convert to binary
+ * @returns {string} a string as a result
+ * @example '01101000 01100101 01101100 01101100 01101111'
+ */
 function bini (text) {
   if (typeof text !== 'string') {
     throw new Error('the argument must be of type string')
--- tests/main.test.js
+++ tests/main.test.js
@@ -3,7 +3,7 @@ const bini = require('../')
 
 test('bini should return a string with the result, something like \'011101 00010...\'', t => {
   const result = bini('hello')
-  const expected = '01101000 01100101 01101100 01101100 01101111'
+  const expected = '01101000 01100101 01101100 01101100 01101111' // precalculated
 
   t.is(result, expected)
 })
