d54d0d04a6d416dc46928a9e60a6b30839c208de
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-09 13:15:40 -0600

    add validation, to handle only strings

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function bini (text) {
+  if (typeof text === 'number') {
+    throw new Error('the argument must be of type string')
+  }
+
   /* eslint-disable camelcase */
   const binary_arr = text.split('').map(character => {
     const unicode = character.charCodeAt()
